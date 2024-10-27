55c430c1a4e3d1fa3942d9122d749d1709984c69
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2024-10-27 19:52:11 +0600

    hm

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -21,7 +21,8 @@
         { name: "MYSQL (MariaDB)" },
         { name: "Redis" },
         { name: "git" },
-        { name: "ClickHouse" }
+        { name: "ClickHouse" },
+        { name: "Kafka" }
       ]
     },
     {
@@ -66,11 +67,16 @@
       <ol class="mt-2 relative border-s border-gray-200 dark:border-gray-700">            
         <li class="mb-10 ms-4">
             <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Since February 2020 to Now</time>
+            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Since February 2020 to August 2024</time>
             <h3 class="text-lg font-semibold dark:text-white">DevOps-engineer at RustMe</h3>
             <p class="mb-4 text-base font-normal text-gray-200 dark:text-gray-400">My responsobilites are creating and maintaining critical infrastructure for <strong class="text-violet-400">game servers</strong> as well as for development critical resources. Basicly monitoring, setting up new servers, networking, handling CI/CD and any kind of automation.</p>
             <a href="https://rustme.net" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Check the server out, it's pretty cool &rarr;</a>
         </li>
+        <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Since August 2024 to Now</time>
+          <h3 class="text-lg font-semibold dark:text-white">DevOps-engineer at Raffeisein Bank</h3>
+      </li>
     </ol>
     </section>
     <!-- Education -->
